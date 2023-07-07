5110f86f8094bfa6fdc5f541efd23f845f10020b
kiplrry <kiplarry19@gmail.com>
2023-07-07 20:44:37 +0300

    new

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let menuBtn  = document.getElementById("menubtn")
 let body = document.body
+let header = document.getElementById("header")
 let menu = document.getElementById("menu_nav")
 function toggleMenu(e){
     if(menu.classList.contains("closedMenu") && e.target == menuBtn ){
@@ -9,5 +10,11 @@ function toggleMenu(e){
     }
 
 }
+// document.addEventListener("scroll", e=>{
+//     let rect = header.getBoundingClientRect()
+//     if(rect.top < -(rect.height)){
+//         header.classList.add("fixedHeader")
+//     }
+// })
 
 body.addEventListener("click", toggleMenu)
